33e1f75c00cb5361e46c539a31b4ae71c47a8574
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2019-04-29 19:13:49 -0300

    update to use emotion 10

--- lib/convertCssForEmotion.ts
+++ lib/convertCssForEmotion.ts
@@ -1,73 +1,74 @@
-import { convertScopedCssForEmotion } from "./convertScopedCssForEmotion";
-import { convertScopeToModuleName } from "./convertScopeToModuleName";
-import { getCssIndexedByScope } from "./getCssIndexedByScope";
-import { getRequiredScopes } from "./getRequiredScopes";
+// tslint:disable no-console
+import {convertScopedCssForEmotion} from './convertScopedCssForEmotion';
+import {convertScopeToModuleName} from './convertScopeToModuleName';
+import {getCssIndexedByScope} from './getCssIndexedByScope';
+import {getRequiredScopes} from './getRequiredScopes';
 
 export function convertCssForEmotion(css: string): string {
-    let cssForEmotion = "";
+  let cssForEmotion = '';
 
-    const cssIndexedByScope = getCssIndexedByScope(css);
+  const cssIndexedByScope = getCssIndexedByScope(css);
 
-    if (cssIndexedByScope.has("root")) {
-        if (cssIndexedByScope.size > 1) {
-            cssForEmotion += 'import { css, injectGlobal } from "emotion";\n';
-        } else {
-            cssForEmotion += 'import { injectGlobal } from "emotion";\n';
-        }
-    } else if (cssIndexedByScope.size > 0) {
-        cssForEmotion += 'import { css } from "emotion";\n';
+  if (cssIndexedByScope.has('root')) {
+    if (cssIndexedByScope.size > 1) {
+      cssForEmotion += 'import { css } from "@emotion/core";\n';
+    } else {
+      cssForEmotion += 'import { css } from "@emotion/core";\n';
     }
+  } else if (cssIndexedByScope.size > 0) {
+    cssForEmotion += 'import { css } from "@emotion/core";\n';
+  }
 
-    const knownScopes = new Set([...cssIndexedByScope.keys()]);
+  const knownScopes = new Set([...cssIndexedByScope.keys()]);
 
-    const collator = new Intl.Collator(undefined, {
-        numeric: true,
-        sensitivity: "base",
-    });
+  const collator = new Intl.Collator(undefined, {
+    numeric: true,
+    sensitivity: 'base',
+  });
 
-    const sortedKnownScopes = [...knownScopes]
-        .sort((scopeA, scopeB) => {
-            if (scopeA === "root") {
-                return -1;
-            }
+  const sortedKnownScopes = [...knownScopes]
+    .sort((scopeA, scopeB) => {
+      if (scopeA === 'root') {
+        return -1;
+      }
 
-            return collator.compare(scopeA, scopeB);
-        })
-        .reduce((previousSortedKnownScopes: Set<string>, knownScope) => {
-            getRequiredScopes(
-                cssIndexedByScope.get(knownScope) as string,
-                knownScope,
-                knownScopes,
-            ).forEach((requiredScope) => {
-                if (previousSortedKnownScopes.has(requiredScope) === false) {
-                    previousSortedKnownScopes.add(requiredScope);
-                }
-            });
-
-            if (previousSortedKnownScopes.has(knownScope) === false) {
-                previousSortedKnownScopes.add(knownScope);
-            }
-
-            return previousSortedKnownScopes;
-        }, new Set());
-
-    sortedKnownScopes.forEach((scope) => {
-        cssForEmotion += "\n";
-
-        const convertedScopedCssForEmotion = convertScopedCssForEmotion(
-            cssIndexedByScope.get(scope) as string,
-            scope,
-            knownScopes,
-        );
-
-        if (scope === "root") {
-            cssForEmotion += `injectGlobal\`${convertedScopedCssForEmotion}\`;\n`;
-        } else {
-            cssForEmotion += `export const ${convertScopeToModuleName(
-                scope,
-            )} = css\`${convertedScopedCssForEmotion}\`;\n`;
+      return collator.compare(scopeA, scopeB);
+    })
+    .reduce((previousSortedKnownScopes: Set<string>, knownScope) => {
+      getRequiredScopes(
+        cssIndexedByScope.get(knownScope) as string,
+        knownScope,
+        knownScopes,
+      ).forEach(requiredScope => {
+        if (previousSortedKnownScopes.has(requiredScope) === false) {
+          previousSortedKnownScopes.add(requiredScope);
         }
-    });
+      });
 
-    return cssForEmotion;
+      if (previousSortedKnownScopes.has(knownScope) === false) {
+        previousSortedKnownScopes.add(knownScope);
+      }
+
+      return previousSortedKnownScopes;
+    }, new Set());
+
+  sortedKnownScopes.forEach(scope => {
+    cssForEmotion += '\n';
+
+    const convertedScopedCssForEmotion = convertScopedCssForEmotion(
+      cssIndexedByScope.get(scope) as string,
+      scope,
+      knownScopes,
+    );
+
+    if (scope === 'root') {
+      cssForEmotion += `export const globalStyle = css\`${convertedScopedCssForEmotion}\`;\n`;
+    } else {
+      cssForEmotion += `export const ${convertScopeToModuleName(
+        scope,
+      )} = css\`${convertedScopedCssForEmotion}\`;\n`;
+    }
+  });
+
+  return cssForEmotion;
 }
